022029a1ff87c093199c77a3e25c482e4c67c083
ste7an-cs <ste7ann@gmail.com>
2022-02-22 22:24:05 +0800

    organised

--- 4.5/main.cpp
+++ 4.5/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
+#include "include/twoSum.h"
+
+
+void isex(std::vector<int> &num1, std::vector<int> &num2);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> num1, num2;
+    num1 = {0,1,1,2};
+    num2 = {0,1,1,2,3,5,8};
+    isex(num1, num2);
+
+
+    std::cout << "#########" << std::endl;
+
+
+    std::vector<int> nums = {1, 2, 3, 6, 10};
+    int m_target = 7;
+    Solution twosum(nums, m_target);
+    twosum.result = twosum.twoSum(twosum);
+    std::cout <<twosum.result[0] << std::endl;
+    std::cout <<twosum.result[1] ;
+
     return 0;
 }
--- 4.5/twoSum.cpp
+++ 4.5/twoSum.cpp
@@ -1,35 +1,12 @@
 //
 // Created by ste7an on 2021/12/10.
 //
-#include <iostream>
-#include <map>
-#include<vector>
-using namespace std;
-
-class Solution{
-public:
-    vector<int> twoSum(Solution n);
-    Solution(vector<int>& a, int b)
-            :   nums(a) , target(b)
-    {}
-
-     vector<int> get_num() const{
-        return nums ;
-    };
-     int get_target() const{
-        return target ;
-    };
+#include"include/twoSum.h"
 
 
-    vector<int>  result;
-
-private:
-     const int target;
-     vector<int> &nums;
-};
- vector<int> Solution::twoSum(Solution n) {
+std::vector<int> Solution::twoSum(Solution n) {
      int target = Solution::get_target();
-     vector<int> num = get_num();
+     std::vector<int> num = get_num();
      map<int,int> hashMap;
      for(int i = 0; i < num.size(); ++i){
         auto it = hashMap.find(target - num[i]);
@@ -37,14 +14,4 @@ private:
         hashMap[num[i]] = i;
     }
      return {};
-}
-
-int main(){
-    vector<int> nums = {1, 2, 3, 6, 10};
-    int target = 7;
-    Solution twosum(nums, target);
-    twosum.result = twosum.twoSum(twosum);
-    cout <<twosum.result[0] ;
-    cout <<twosum.result[1] ;
-
 }
