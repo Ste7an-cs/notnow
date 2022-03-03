929a40145e5d283a01f29b38f8783106add8cb43
ste7an-cs <ste7ann@gmail.com>
2022-03-03 23:11:52 +0800

    6.1

--- 6.x/main.cpp
+++ 6.x/main.cpp
@@ -9,6 +9,6 @@ int main(int argc, const char **argv) {
     for (int i = 0;i != argc; i++ ){
         std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
     }
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
