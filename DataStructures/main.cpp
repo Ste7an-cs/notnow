e136553da361ba963acc301431d79d5eb0e8278b
ste7an <ste7ann@gmail.com>
2022-03-06 22:53:23 +0800

    vector0.1

--- DataStructures/main.cpp
+++ DataStructures/main.cpp
@@ -1,17 +1,97 @@
 #include <iostream>
 #include <vector>
 #include "vector.h"
-int main()
-{
-    vector<int> data;
-    data.pushBack(2);
 
-    data.pushBack(3);
-    data.pushBack(4);
-    data.pushBack(44);
-    //range based loop
-    for(size_t i = 0; i < data.Size(); i++){
-        std::cout << data[i] << std::endl;
+struct Vector3
+{
+    float x = 0.0f, y = 0.0f, z = 0.0f;
+    int* m_MemoryBlock = nullptr;
+    Vector3(){
+        m_MemoryBlock = new int[5];
+    }
+    Vector3(float scalar)
+    : x(scalar), y(scalar), z(scalar){
+        m_MemoryBlock = new int[5];
+    }
+    Vector3(float x, float y, float z)
+    : x(x), y(y), z(z) { m_MemoryBlock = new int[5];}
+
+    Vector3(const Vector3& other)
+        : x(other.x), y(other.y), z(other.z)
+    {
+        std::cout << "Copy\n";
+    }
+    Vector3( Vector3&& other) noexcept
+        : x(other.x), y(other.y), z(other.z)
+    {
+        m_MemoryBlock = other.m_MemoryBlock;
+        other.m_MemoryBlock = nullptr;
+        std::cout << "Move\n";
+    }
+
+
+    ~Vector3()
+    {
+        delete[] m_MemoryBlock;
+        std::cout << "Vector3 Destory\n";
+    }
+    Vector3& operator=(const Vector3& other)
+    {
+        std::cout << "Copy assignment\n";
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        return *this;
+    }
+
+    Vector3& operator=( Vector3&& other)
+ noexcept     {
+        m_MemoryBlock = other.m_MemoryBlock;
+        other.m_MemoryBlock = nullptr;
+        std::cout << "Move assignment\n";
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        return *this;
+
+    }
+};
+
+template<typename T>
+void PrintVector(const vector<T>& v)
+{
+    for(size_t i=0; i<v.Size(); i++)
+    {
+        std::cout << v[i] << std::endl;
     }
     std::cout << "-------------" << std::endl;
+}
+
+template<>
+void PrintVector(const vector<Vector3>& v)
+{
+    for(size_t i=0; i<v.Size(); i++)
+    {
+        std::cout << v[i].x << "," << v[i].y << "," << v[i].z << std::endl;
+    }
+    std::cout << "-------------" << std::endl;
+}
+int main()
+{
+    vector<Vector3> data;
+    data.pushBack(Vector3(1.0f));
+    data.pushBack(Vector3(2, 3, 4));
+    data.pushBack(Vector3());
+    PrintVector(data);
+    data.PopBack();
+    data.PopBack();
+    data.EmplaceBack();
+    data.EmplaceBack(1,2,3);
+    data.EmplaceBack(12,21,31);
+    PrintVector(data);
+    data.Clear();
+    PrintVector(data);
+    data.EmplaceBack(1,2,3);
+    data.EmplaceBack(12,21,31);
+    PrintVector(data);
 }
--- DataStructures/vector.h
+++ DataStructures/vector.h
@@ -11,50 +11,94 @@ class vector
 {
 public:
     vector(){
-        Realloc(2);
+        //allocate 2 elements
+        //ReAlloc(2);
     }
 
     ~vector()
     {
+        std::cout << " vector Destory" << std::endl;
+        Clear();
+        ::operator delete(m_Data, m_Capacity * sizeof(T));
+    }
 
-
-
+    void pushBack( T&& value)
+    {
+        if(m_Size >= m_Capacity)
+            ReAlloc(m_Capacity + m_Capacity / 2);
+        m_Data[m_Size] = std::move(value);
+        m_Size ++;
     }
 
     void pushBack(const T& value)
     {
         if(m_Size >= m_Capacity)
-            Realloc(m_Capacity + m_Capacity / 2);
+            ReAlloc(m_Capacity + m_Capacity / 2);
         m_Data[m_Size] = value;
         m_Size ++;
     }
 
-    size_t Size() {return m_Size; }
+    template<typename... Args>
+    T& EmplaceBack(Args&&... args)
+    {
+        if(m_Size >= m_Capacity)
+            ReAlloc(m_Capacity + m_Capacity / 2);
+        //placement new
+        std::cout << " Placement new" << std::endl;
+        new (&m_Data[m_Size]) T(std::forward<Args>(args)...);
 
-    T operator[](size_t index){return m_Data[index] ;}
+        //m_Data[m_Size] = T(std::forward<Args>(args)...);
+        return m_Data[m_Size++];
+    }
+    constexpr size_t Size() const {return m_Size; }
 
-    private:
-    void Realloc(size_t newCapacity)
+    T& operator[](size_t index) {return m_Data[index] ;}
+    const T& operator[](size_t index) const  {return m_Data[index] ;}
+
+    void PopBack()
+    {
+        std::cout << "Pop Back" << std::endl;
+        if (m_Size > 0)
+        {
+            m_Size--;
+            m_Data[m_Size].~T();
+        }
+
+    }
+    void Clear()
+    {
+        for(size_t i=0; i < m_Size; ++i){
+            m_Data[i].~T();
+        }
+        m_Size = 0;
+        std::cout << "delete everything" << std::endl;
+    }
+private:
+    void ReAlloc(size_t newCapacity)
     {
         //1. allocate a new block of memory
         //2. copy/move old elements into new block
         //3. delete
         T* newBlock = (T*)::operator new(newCapacity * sizeof(T));
-
         if (newCapacity < m_Size) m_Size = newCapacity;
+
+        //move
         for(size_t i = 0; i < m_Size; i++)
             newBlock[i] = std::move(m_Data[i]);
+        //delete
+        std::cout << " realloc" << std::endl;
         for(size_t i = 0; i < m_Size; i++)
             m_Data[i].~T();
+        //set
         ::operator delete(m_Data, m_Capacity * sizeof(T));
         m_Data = newBlock;
         m_Capacity = newCapacity;
     }
 
 private:
-    T* m_Data = nullptr;
+    T* m_Data = (T*)::operator new(m_Capacity * sizeof(T));
     size_t m_Size = 0;
-    size_t m_Capacity = 0 ;
+    size_t m_Capacity = 2;
 };
 
 
