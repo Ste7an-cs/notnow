a7a9f8c7a5e98f6eeb5f6764b5542f9ea6f7169d
ste7an <ste7ann@gmail.com>
2022-03-17 21:22:56 +0800

    allocator construct

--- TinyStl/allocator.h
+++ TinyStl/allocator.h
@@ -4,10 +4,13 @@
 
 #ifndef TINYSTL_ALLOCATOR_H
 #define TINYSTL_ALLOCATOR_H
-namespace TINYSTL
+
+#include"construct.h"
+
+namespace tinystl
 {
 template <class T>
-class Allocator
+class allocator
 {
 public:
     typedef T               value_type;
@@ -20,17 +23,96 @@ public:
 
 public:
     static T* allocate();
-    static T* allocate(size_t size);
+    static T* allocate(size_type n);
 
     static void deallocate(T* ptr);
-    static void deallocate(T* ptr, size_t size);
+    static void deallocate(T* ptr, size_type size);
 
     static void construct(T* ptr);
-    
+    static void construct(T* ptr, const T& value);      //placement new 在pty位置call T.copy_ctor构造对像
+    static void construct(T* ptr, T&& value);           //placement new  call T.move_ctor
+
+    template<class ...Args>
+    static void construct(T* ptr, Args&& ...args);       //placement new by parameter pack
+
+    static void destroy(T* ptr);                        // destroy one
+    static void destroy(T* first, T* last);             //destroy from to last
+
+
 };
 
+
+template<class T>
+T* allocator<T>::allocate()
+{
+    return static_cast<T*>(::operator new(sizeof(T)));
+}
+
+template<class T>
+T* allocator<T>::allocate(size_type n)
+{
+    if (n == 0)
+        return nullptr;
+    return static_cast<T*>(::operator new(n * sizeof(T)));
+}
+
+template<class T>
+void allocator<T>::deallocate(T* ptr)
+{
+    if(ptr == nullptr)
+        return;
+    ::operator delete(ptr);
+}
+
+template<class T>
+void allocator<T>::deallocate(T* ptr ,size_type size)
+{
+    if(ptr == nullptr)
+        return;
+    ::operator delete(ptr);
+}
+
+
+template<class T>
+void allocator<T>::construct(T* ptr)
+{
+    tinystl::construct(ptr);
+}
+
+template<class T>
+void allocator<T>::construct(T *ptr, T &&value)
+{
+    tinystl::construct(ptr, std::move(value));
+}
+template<class T>
+template< class... Args>
+void allocator<T>::construct(T *ptr, Args &&...args)
+{
+    tinystl::construct(ptr, std::forward<Args>(args)...);
+}
+
+template<class T>
+void allocator<T>::construct(T *ptr, const T &value)
+{
+    tinystl::construct(ptr, value);
+}
+
+template<class T>
+void allocator<T>::destroy(T* ptr)
+{
+    tinystl::destroy(ptr);
+}
+
+template<class T>
+void allocator<T>::destroy(T* first, T* last)
+{
+    tinystl::destroy(first, last);
+}
 
 
 
 }
+
+
+} //tinystl
 #endif //TINYSTL_ALLOCATOR_H
--- TinyStl/construct.h
+++ TinyStl/construct.h
@@ -1,8 +1,83 @@
 //
 // Created by ste7an on 2022/3/16.
 //
-
+/*
+ *  该头文件包含两个函数：construct、destroy
+ *  construct：负责构造对象
+ *  destroy：负责析构对象
+ *
+ */
 #ifndef TINYSTL_CONSTRUCT_H
 #define TINYSTL_CONSTRUCT_H
 
+#include "iterator.h"
+
+namespace tinystl
+{
+    //construct
+
+template<class T>
+void construct(T* ptr)
+{
+    ::new((void*)ptr) T();
+}
+
+template<class T, class U>
+void construct(T* ptr, U& val)
+{
+    ::new((void*)ptr) U(val);
+}
+
+template<class T, class ...Args>
+void construct(T* ptr, Args&&... args)
+{
+    ::new((void*)ptr) T(std::forward<Args>(args)... );
+}
+
+
+//destroy
+
+
+template<class T>
+void _destroy_Ptr(T* , std::true_type){}
+
+template<class T>
+void _destroy_Ptr(T* ptr, std::false_type)
+{
+    if (ptr!=nullptr)
+    {
+        ptr->~T();
+    }
+}
+
+template<class T>
+void destroy(T* ptr)
+{
+    _destroy_Ptr(ptr, std::is_trivially_destructible<T>{});
+}
+
+
+template<class ForwardIter>
+void _destroy_Iter(ForwardIter , ForwardIter , std::true_type){}
+
+template<class ForwardIter>
+void _destroy_Iter(ForwardIter first, ForwardIter last, std::false_type)
+{
+    for(; first!=last; ++first)
+    {
+        destroy(&*first);
+    }
+}
+
+template<class ForwardIter>
+void destroy(ForwardIter first, ForwardIter last)
+{
+    _destory_Iter(first, last, std::is_trivially_destructible<
+            typename IteratorTraits<ForwardIter>::value_type>{});
+}
+
+
+
+
+}//namespace tinystl
 #endif //TINYSTL_CONSTRUCT_H
