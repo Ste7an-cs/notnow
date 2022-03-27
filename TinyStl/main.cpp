c4e35ddc0f268db92e1744b4b8514bd41c500920
Ste7an-cs <ste7ann@gmail.com>
2022-03-27 14:45:34 +0800

    test

--- TinyStl/main.cpp
+++ TinyStl/main.cpp
@@ -2,7 +2,53 @@
 #include "iterator.h"
 #include "allocator.h"
 #include "vector.h"
+#include <vector>
+#include <string>
+struct Person
+{
+public:
+
+    Person(int age, std::string name)
+            :m_age(age), m_name(std::move(name))
+    {
+        std::cout<< "i am constructed" << std::endl;
+    }
+
+    Person(const Person& other)
+            :m_age(other.m_age), m_name(other.m_name)
+    {
+        std::cout<< "i am copyed" << std::endl;
+    }
+
+    Person(Person&& other) noexcept
+    :m_age(other.m_age), m_name(other.m_name)
+
+    {
+        std::cout<< "i am moved" << std::endl;
+    }
+    ~ Person()
+    {
+        std::cout<< "destroy" << std::endl;
+    }
+private:
+    int m_age;
+    std::string m_name;
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::vector<Person> students;
+    Person kate(18, "kate");
+//    std::cout<< "emplace back: lvalue" << std::endl;
+//    students.emplace_back(kate);
+
+
+    std::cout<< "push back: rvalue" << std::endl;
+//    students.push_back(Person(18, "kate"));
+    students.push_back(kate);
+
+
+
     return 0;
 }
